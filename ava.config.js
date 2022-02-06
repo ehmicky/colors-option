d11bd950952f9d641f5cb70c365d6d9743f31a94
ehmicky <ehmicky@gmail.com>
2022-02-06 15:42:08 +0100

    Fix tests

--- ava.config.js
+++ ava.config.js
@@ -1,1 +1,7 @@
-export { default } from '@ehmicky/dev-tasks/ava.config.js'
+import avaConfig from '@ehmicky/dev-tasks/ava.config.js'
+
+export default {
+  ...avaConfig,
+  // Testing `stdout.fd` in this module is simpler with processes
+  workerThreads: false,
+}
