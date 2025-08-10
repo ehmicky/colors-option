5dc239eba92df4010d51c4fc003d4bb34dca26d0
ehmicky <ehmicky@gmail.com>
2025-08-10 22:19:56 +0100

    Update comments

--- src/main.js
+++ src/main.js
@@ -29,8 +29,12 @@ const getLevel = (colors, stream) => {
   return colors === undefined ? terminalLevel : Math.max(terminalLevel, 1)
 }
 
+// The internal Node.js code actually does not use `stream` but `process.env`
+// to determine the color depth:
+// https://github.com/nodejs/node/blob/aac7925801edfc8dd1de051a29aac85332e7d200/lib/internal/tty.js#L123
+// We still need the `stream` to disable colors by default when it is not a TTY.
 const getTerminalLevel = (stream) =>
   stream.isTTY ? DEPTH_TO_LEVEL[stream.getColorDepth()] : 0
 
-// Maps chalk levels to color depth
+// Maps color depth to chalk levels
 const DEPTH_TO_LEVEL = { 1: 0, 4: 1, 8: 2, 24: 3 }
